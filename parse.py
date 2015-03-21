4ea405f383514533e5d71c37aed204e8cd96ec63
preich <preich@192.168.1.101>
2015-03-21 18:39:40 +0000

    MPs to constituencies

---
+++ parse.py
@@ -0,0 +1,17 @@
+import io
+import time
+import re
+from lxml import html
+import requests
+import sys
+sys.setdefaultencoding('utf-8')
+
+writer = open('result.csv', 'w')
+data = open('index.html').read()
+tree = html.fromstring(data)
+
+items = tree.xpath('//tr[contains(@id, "trItemRow")]/td/text()')
+items = [x.rstrip().lstrip() for x in items] 
+for i in range(1,len(items),3):
+  line = items[i+1] + "," + items[i]
+  writer.write(line+"\n")
