de7b02649b5974ed517292e0570189b7d2a9d482
preich <preich@192.168.1.101>
2015-03-21 18:59:06 +0000

    life expectancy from sheet CB

---
+++ lifeexp.R
@@ -0,0 +1,8 @@
+library(gdata)
+data <- read.xls("~/Downloads/referencetable1_tcm77-332890.xls",sheet=18,header=F)
+ret <- data[grep("^E[0-9]+$", data$V1),c("V1","V81")]
+names(ret) <- c("area","le")
+ret$le <- as.numeric(as.vector(ret$le))
+ret <- ret[!is.na(ret$le),]
+head(ret)
+write.csv(ret,"lifeexp-male-2010.csv")
