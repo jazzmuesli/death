893466b71a728b30725c40aa2821c7dbfe60b027
preich <preich@192.168.1.101>
2015-03-22 14:10:08 +0000

    0.0 for the LSOA with lowest life expectancy and 1.0 for the best, relative to year

--- generate-json.py
+++ generate-json.py
@@ -4,13 +4,24 @@ import csv
 
 def fix_text(str):
   return str.decode('ascii','ignore')
-
+def derive_party(party):
+  parties = ["conservative","labour","liberal","respect"]
+  for p in parties:
+    if p in party.lower():
+      return p
+  if "Speaker" in party:
+    return 'conservative'
+  return party
 lifeexp = dict()
 with open('lifeexp-1991-2010.csv', 'rb') as csvfile:
   spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+  i = 0
   for row in spamreader:
+    i = i+1
+    if i == 1:
+      continue
     area = row[1]
-    year = row[2]
+    year = int(row[2])
     gender = row[3]
     le = row[4]
     lifeexp[area] = lifeexp.get(area, dict())
@@ -26,26 +37,46 @@ with open('party-year-area.csv', 'rb') as csvfile:
     if i == 1:
       continue
     area = row[1]
-    year = row[2]
-    party = fix_text(row[3])
+    year = int(row[2])
+    party = derive_party(fix_text(row[3]))
     cnt = row[4]
     mps[area] = mps.get(area, dict())
     mps[area][year] = mps[area].get(year, dict())
     mps[area][year][party] = cnt
 
+le_map = dict()
 for area in lifeexp:
   for year in lifeexp[area]:
     les = [float(lifeexp[area][year].get(x,0)) for x in ['male','female']]
     if len(les) == 2:
-      lifeexp[area][year] = "%.1f" % float(sum(les)/2)
-
+      le = float("%.1f" % float(sum(les)/2))
+      x = le_map.get(year, [])
+      x.append(le)
+      le_map[year]= x
+      lifeexp[area][year] = le
+for area in lifeexp:
+  for year in lifeexp[area]:
+    x = lifeexp[area][year]
+    #g = (x-min(le_list))/(max(le_list)-min(le_list))
+    g = (x - min(le_map[year])) / (max(le_map[year])-min(le_map[year]))
+    #print x, g, min(le_list), max(le_list)
+    lifeexp[area][year] = float("%.2f" % g)
+    
+area_seats = dict()
 for area in mps:
   for year in mps[area]:
     seats = [int(x) for x in mps[area][year].values()]
     for party in mps[area][year]:
-      mps[area][year][party] = "%.2f" % (float(mps[area][year][party]) / sum(seats))
+      proportion = float("%.2f" % (float(mps[area][year][party]) / sum(seats)))
+      mps[area][year][party] = proportion
+      area_seats[area] = area_seats.get(area, [])
+      area_seats[area].append(proportion)
       #mps[area][year][party] = "%.2f" % float(mps[area][year][party])/sum(seats)
     #print seats, sum(seats)
+#for area in area_seats:
+#  seats = area_seats[area]
+#  print sum(seats)/len(seats) < 1.0
+#print json.dumps(area_seats, indent=True)
 #sys.exit(0)
 data = json.loads(file('lad.json').read())
 features = data['features']
@@ -54,7 +85,12 @@ for i in range(len(features)):
   area = feature['properties']['LAD13CD']
   feature['properties']['lifeexp'] = lifeexp.get(area, dict())
   feature['properties']['mps'] = mps.get(area, dict())
+  x = feature['properties']['mps']
+  #for year in x:
+    #print "\n".join(x[year].keys())
+  #print ["\n".join(x.keys()) for x in feature['properties']['mps'].values()]
 #print mps#, indent=True)
+
 text = json.dumps(data, indent=True) 
-print text
+print "var  localAuthorities =", text + ";"
 #print text.encode('ascii','ignore')
