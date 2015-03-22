f62f9fe4c63395c275efa53a6fd30dbfc8c052d7
preich <preich@192.168.1.101>
2015-03-22 10:14:05 +0000

    added female.le/male.le

--- mix.R
+++ mix.R
@@ -1,16 +1,13 @@
 options(stringsAsFactors = FALSE)
 
-lifeexp <- read.csv("lifeexp-male-2010.csv")
+lifeexp <- read.csv("lifeexp-2010.csv")
 mps <- read.csv("result.csv",header=F)
-#names(mps) <- c("")
 lad <- read.csv("LAD11_PCON11_UK_LU.csv")
 leconst <- merge(lifeexp, lad, by.x="area", by.y="LAD11CD")
 m <- merge(leconst,mps, by.x="PCON11NM",by.y="V1")
 
-m <- m[,c("PCON11NM","area","le","V2")]
-names(m) <- c("const","area","le","Party.Name")
-
-#plot(m$V2, m$le)
+m <- m[,c("PCON11NM","area","male.le","female.le","V2")]
+names(m) <- c("const","area","male.le","female.le","Party.Name")
 
 spend <- read.csv("2010-UK-Parliament-spending-data-CSV.csv")
 spend <- spend[,c("Constituency.Name","Position","Votes..","Party.Name")]
