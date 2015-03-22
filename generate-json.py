3cd55d4b8cdc4c74ffaeeb2ead9352d95bdedef9
preich <preich@192.168.1.101>
2015-03-22 12:10:22 +0000

    averaged male/female

--- generate-json.py
+++ generate-json.py
@@ -13,10 +13,15 @@ with open('lifeexp-1991-2010.csv', 'rb') as csvfile:
     lifeexp[area][year] = lifeexp[area].get(year, dict())
     lifeexp[area][year][gender] = le
 
+for area in lifeexp:
+  for year in lifeexp[area]:
+    les = [float(lifeexp[area][year].get(x,0)) for x in ['male','female']]
+    if len(les) == 2:
+      lifeexp[area][year] = "%.1f" % float(sum(les)/2)
 data = json.loads(file('lad.json').read())
 features = data['features']
 for i in range(len(features)):
   feature = features[i]
   area = feature['properties']['LAD13CD']
-  feature['lifeexp'] = lifeexp.get(area, dict())
+  feature['properties']['lifeexp'] = lifeexp.get(area, dict())
 print json.dumps(data, indent=True) 
