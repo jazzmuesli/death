120ba145b184b7f3e1f5b797a18036031de2e864
preich <preich@192.168.1.101>
2015-03-22 10:11:54 +0000

    female added

--- lifeexp.R
+++ lifeexp.R
@@ -1,9 +1,15 @@
 library(gdata)
-data <- read.xls("~/Downloads/referencetable1_tcm77-332890.xls",sheet=18,header=F)
-ret <- data[grep("^E[0-9]+$", data$V1),c("V1","V81")]
-names(ret) <- c("area","le")
-ret$le <- as.numeric(as.vector(ret$le))
-ret <- ret[!is.na(ret$le),]
-head(ret)
-write.csv(ret,"lifeexp-male-2010.csv")
+getLifeExp <- function(s, type) {
+  data <- read.xls("~/Downloads/referencetable1_tcm77-332890.xls",sheet=s,header=F)
+  ret <- data[grep("^E[0-9]+$", data$V1),c("V1","V80")]
+  names(ret) <- c("area", "le")
+  ret$le <- as.numeric(as.vector(ret$le))
+  ret <- ret[!is.na(ret$le),]
+  names(ret)[2] <- paste0(type, ".le")
+  ret
+}
+male <- getLifeExp(18,"male")
+female <- getLifeExp(22,"female")
+ret <- merge(male,female)
+write.csv(ret,"lifeexp-2010.csv")
 
